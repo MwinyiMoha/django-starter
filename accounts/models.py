b084dc1d54a4ed43f2ecb3138025bbe7300990b8
Mohammed Mwijaa <mm.mwijaa@gmail.com>
2020-12-04 13:26:38 +0300

    Set up custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = [
+        "username",
+    ]
+
+    def __str__(self):
+        return self.username
+
+    def save(self, *args, **kwargs):
+        if not self.username:
+            name_str = self.email.split("@")[0]
+            if self.first_name and self.last_name:
+                name_str = self.get_full_name()
+
+            self.username = slugify(name_str)
+
+        super(User, self).save(*args, **kwargs)
--- core/settings/base.py
+++ core/settings/base.py
@@ -79,3 +79,9 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+
+# Custom user model settings
+# https://docs.djangoproject.com/en/2.2/topics/auth/customizing/
+
+AUTH_USER_MODEL = "accounts.User"
