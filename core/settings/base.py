979378d4ffe3dab1bf2e85eee4af19cbbada5003
Mohammed Mwijaa <mm.mwijaa@gmail.com>
2020-12-04 14:34:29 +0300

    Install and set up django rest auth

--- core/settings/base.py
+++ core/settings/base.py
@@ -33,6 +33,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework.authtoken",
+    "rest_auth",
     "accounts",
 ]
 
