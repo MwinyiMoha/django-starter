5266efe8c2b10b9b15e78f5c3962fbf7cf971001
Mohammed Mwijaa <mm.mwijaa@gmail.com>
2020-12-04 14:17:04 +0300

    Minor fixes

--- core/settings/staging.py
+++ core/settings/staging.py
@@ -16,6 +16,6 @@ ALLOWED_HOSTS = []
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES["default"] = dj_database_url.config(
-    conn_max_age=600, ssl_require=True
-)
+DATABASES = {
+    "default": dj_database_url.config(conn_max_age=600, ssl_require=True)
+}
